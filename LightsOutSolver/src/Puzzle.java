0ed53038a76b3350eab690c3a5882d2205a6d724
bowserm44 <bowserm44@gmail.com>
2012-01-13 18:57:25 +0000

    Initial checkin of puzzle class and solver.

---
+++ LightsOutSolver/src/Puzzle.java
@@ -0,0 +1,106 @@
+public class Puzzle {
+	public boolean[][] _grid;
+	private int _height, _width;
+	
+	public Puzzle(int width, int height)
+	{
+		_height = height;
+		_width = width;
+		_grid = new boolean[_height][_width];
+		for(int y = 0; y < _height; y++)
+		{
+			for(int x = 0; x < _width; x++)
+			{
+				_grid[y][x] = false;
+			}
+		}
+	}
+	
+	public Puzzle(Puzzle toCopy)
+	{
+		_height = toCopy.GetHeight();
+		_width = toCopy.GetWidth();
+		_grid = new boolean[_height][_width];
+		
+		for(int y = 0; y < _height; y++)
+		{
+			for(int x = 0; x < _width; x++)
+			{
+				_grid[y][x] = toCopy._grid[y][x];
+			}
+		}
+		PrintPuzzle();
+	}
+	
+	public int GetHeight()
+	{
+		return _height;
+	}
+	
+	public int GetWidth()
+	{
+		return _width;
+	}
+
+	public Puzzle PlayCoord(int x, int y)
+	{
+		if( x >=_width || x < 0 ){
+			System.out.println("X coord was not in bounds of puzzle");
+			return null;
+		}
+		
+		if( y >=_height || y < 0 ){
+			System.out.println("Y coord was not in bounds of puzzle");
+			return null;
+		}
+		
+		Puzzle newPuz = new Puzzle(this);
+		newPuz.ToggleCoordAndNeighbors(x, y);
+		return newPuz;
+	}
+	
+	public void ToggleCoordAndNeighbors(int x, int y)
+	{
+		_grid[y][x] = !_grid[y][x];
+		if(y < _height-1)
+			_grid[y+1][x] = !_grid[y+1][x];
+		if(x < _width-1)
+			_grid[y][x+1] = !_grid[y][x+1];
+		if(y > 0)
+			_grid[y-1][x] = !_grid[y-1][x];
+		if(x > 0)
+			_grid[y][x-1] = !_grid[y][x-1];
+	}
+	
+	public boolean IsPuzzleDone()
+	{
+		int trueCount = 0;
+		for(int y = 0; y < _height; y++)
+		{
+			for(int x = 0; x < _width; x++)
+			{
+				if(_grid[y][x])
+					trueCount++;
+			}
+		}
+		return (trueCount == _height*_width);
+	}
+	
+	public void PrintPuzzle()
+	{
+		System.out.println("-----------------------------");
+		for(int y = 0; y < _height; y++)
+		{
+			for(int x = 0; x < _width; x++)
+			{
+				if(_grid[y][x])
+					System.out.print('1');
+				else
+					System.out.print('0');
+			}
+			System.out.println();
+		}
+		System.out.println("-----------------------------");
+		System.out.println("                             ");
+	}
+}
---
+++ LightsOutSolver/src/Run.java
@@ -0,0 +1,9 @@
+
+public class Run {
+
+	public static void main(String[] args)
+	{
+		Solver test = new Solver();
+		test.PrintSolution();
+	}
+}
